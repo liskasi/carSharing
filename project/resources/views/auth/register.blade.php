8174b456b1d49adfdfc11cd96c13c8019b753c6a
liskasi <77645171+liskasi@users.noreply.github.com>
2021-06-10 23:55:28 +0300

    - UI package - Contollers/Auth was added (Login and Register) - New Controllers were added - Home.blade.php was edited - pages/addCar.blade.php and pages/myCars.bladed.php were created

---
+++ project/resources/views/auth/register.blade.php
@@ -0,0 +1,101 @@
+@extends('layouts.app', [
+    'namePage' => 'Register page',
+    'activePage' => 'register',
+    'backgroundImage' => asset('assets') . "/img/bg16.jpg",
+])
+
+@section('content')
+  <div class="content">
+    <div class="container">
+      <div class="row ">
+        <div class="col-md-7  mr-auto "style="float: none; margin: 0 auto;">
+          <div class="card card-signup text-center">
+            <div class="card-header ">
+              <h4 class="card-title">{{ __('Register') }}</h4>
+              <div class="social">
+                <button class="btn btn-icon btn-round btn-google">
+                  <i class="fab fa-google"></i>
+                </button>
+                <button class="btn btn-icon btn-round btn-facebook">
+                  <i class="fab fa-facebook-f"></i>
+                </button>
+                <h5 class="card-description">  {{ __('or be classical') }}</h5>
+              </div>
+            </div>
+            <div class="card-body ">
+              <form method="POST" action="{{ route('register') }}">
+                @csrf
+                <!--Begin input name -->
+                <div class="input-group {{ $errors->has('name') ? ' has-danger' : '' }}">
+                  <div class="input-group-prepend">
+                    <div class="input-group-text">
+                      <i class="now-ui-icons users_circle-08"></i>
+                    </div>
+                  </div>
+                  <input class="form-control {{ $errors->has('name') ? ' is-invalid' : '' }}" placeholder="{{ __('Name') }}" type="text" name="name" value="{{ old('name') }}" required autofocus>
+                  @if ($errors->has('name'))
+                    <span class="invalid-feedback" style="display: block;" role="alert">
+                      <strong>{{ $errors->first('name') }}</strong>
+                    </span>
+                  @endif
+                </div>
+                <!--Begin input email -->
+                <div class="input-group {{ $errors->has('email') ? ' has-danger' : '' }}">
+                  <div class="input-group-prepend">
+                    <div class="input-group-text">
+                      <i class="now-ui-icons ui-1_email-85"></i>
+                    </div>
+                  </div>
+                  <input class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('Email') }}" type="email" name="email" value="{{ old('email') }}" required>
+                 </div>
+                 @if ($errors->has('email'))
+                    <span class="invalid-feedback" style="display: block;" role="alert">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
+                <!--Begin input user type-->
+                
+                <!--Begin input password -->
+                <div class="input-group {{ $errors->has('password') ? ' has-danger' : '' }}">
+                  <div class="input-group-prepend">
+                    <div class="input-group-text">
+                      <i class="now-ui-icons objects_key-25"></i>
+                    </div>
+                  </div>
+                  <input class="form-control {{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="{{ __('Password') }}" type="password" name="password" required>
+                  @if ($errors->has('password'))
+                    <span class="invalid-feedback" style="display: block;" role="alert">
+                      <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                  @endif
+                </div>
+                <!--Begin input confirm password -->
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <div class="input-group-text">
+                      <i class="now-ui-icons objects_key-25"></i></i>
+                    </div>
+                  </div>
+                  <input class="form-control" placeholder="{{ __('Confirm Password') }}" type="password" name="password_confirmation" required>
+                </div>
+                <div class="form-check text-left">
+                </div>
+                <div class="card-footer ">
+                  <button type="submit" class="btn btn-primary btn-round btn-lg">{{__('Get Started')}}</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
+
+@push('js')
+  <script>
+    $(document).ready(function() {
+      demo.checkFullPageBackgroundImage();
+    });
+  </script>
+@endpush
